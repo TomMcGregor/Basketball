2628a994922b9863acf306e1b9acaddbd905fb9d
windmillrobotics <zdac099@live.rhul.ac.uk>
2016-12-01 14:50:47 +0000

    Add files via upload
    shit what we did

--- Basketball.java
+++ Basketball.java
@@ -17,6 +17,11 @@ public class Basketball {
 	static DifferentialPilot power = new DifferentialPilot(DIAMETER, AXLE, Motor.A, Motor.C);
 	static UltrasonicSensor us = new UltrasonicSensor(SensorPort.S2);
 	static LightSensor ls = new LightSensor(SensorPort.S4);
+	public static boolean DETECTED = false;
+	public static boolean FIRING = false;    
+	public static boolean BLACK = false;
+	public static boolean WHITE = false;
+	public static boolean GREY = false;
 
 	public static void main(String[] args) {
 		LCD.drawString("High", 1, 1);
@@ -29,7 +34,10 @@ public class Basketball {
 		LCD.clear();
 		Behavior b1 = new Forward();
 		Behavior b2 = new Rotate();
-		Behavior[] bArray = {b1, b2};
+		Behavior b3 = new DetectHoop();
+		Behavior b4 = new SeeHoop();
+		Behavior b5 = new Fire();
+		Behavior[] bArray = {b1, b2,b3,b4,b5};
 		Arbitrator arby = new Arbitrator(bArray);
 		arby.start();		
 		
--- Rotate.java
+++ Rotate.java
@@ -1,43 +1,36 @@
 
 import lejos.robotics.subsumption.Behavior;
 
-
 public class Rotate implements Behavior {
-    static double ROTATEANGLE; 
-    static double ROTATEDIRECTION;
-    static long time = 0;
+	static double ROTATEANGLE;
+	static double ROTATEDIRECTION;
+	static long time = 0;
 
 	public Rotate() {
-		// TODO Auto-generated constructor stub
+
 	}
 
-	
 	public boolean takeControl() {
 		// TODO Auto-generated method stub
-		if (Basketball.ls.getLightValue() > 50)
-		{
+		if (Basketball.ls.getLightValue() > 50) {
 			return true;
-		}
-		else
-		{
-		return false;
+		} else {
+			return false;
 		}
 	}
 
-	
 	public void action() {
 		Basketball.power.travel(-100);
 		ROTATEANGLE = Math.random() * 90 + 90;
 		ROTATEDIRECTION = Math.random();
-		if (ROTATEDIRECTION > 0.5)
-		{
+		if (ROTATEDIRECTION > 0.5) {
 			ROTATEANGLE *= -1;
 		}
+		Basketball.power.setRotateSpeed(360);
 		Basketball.power.rotate(ROTATEANGLE);
 
 	}
 
-	
 	public void suppress() {
 		Basketball.power.stop();
 
