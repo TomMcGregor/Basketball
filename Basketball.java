0bf7e607446d6081ae1331cbc4b2bb0a81eb6160
windmillrobotics <zdac099@live.rhul.ac.uk>
2016-12-07 21:36:26 +0000

    Add files via upload

--- Basketball.java
+++ Basketball.java
@@ -1,46 +1,92 @@
-
 import lejos.nxt.Button;
+
 import lejos.nxt.LCD;
+
 import lejos.nxt.LightSensor;
+
 import lejos.nxt.Motor;
+
 import lejos.robotics.navigation.DifferentialPilot;
+
 import lejos.robotics.subsumption.Behavior;
+
 import lejos.nxt.SensorPort;
+
 import lejos.nxt.UltrasonicSensor;
+
 import lejos.robotics.subsumption.Arbitrator;
 
-
-
 public class Basketball {
+
 	final static float DIAMETER = 41;
+
 	final static float AXLE = 133;
+
 	static DifferentialPilot power = new DifferentialPilot(DIAMETER, AXLE, Motor.A, Motor.C);
+
 	static UltrasonicSensor us = new UltrasonicSensor(SensorPort.S2);
+
 	static LightSensor ls = new LightSensor(SensorPort.S4);
+
 	public static boolean DETECTED = false;
-	public static boolean FIRING = false;    
-	public static boolean BLACK = false;
-	public static boolean WHITE = false;
-	public static boolean GREY = false;
+
+	public static boolean FIRING = false;
+	public static boolean WHITE = true;
+	public static boolean GREY = true;
+	public static boolean END = false;
+	public static boolean DC = true;
+	public static int GREYCOLOR;
 
 	public static void main(String[] args) {
-		LCD.drawString("High", 1, 1);
+
+		LCD.drawString("Black", 1, 1);
+
 		Button.ENTER.waitForPressAndRelease();
+
 		ls.calibrateHigh();
+
 		LCD.clear();
-		LCD.drawString("Low", 1, 1);
-		Button.ENTER.waitForPressAndRelease();
-		ls.calibrateLow();
-		LCD.clear();
-		Behavior b1 = new Forward();
-		Behavior b2 = new Rotate();
-		Behavior b3 = new DetectHoop();
-		Behavior b4 = new SeeHoop();
-		Behavior b5 = new Fire();
-		Behavior[] bArray = {b1, b2,b3,b4,b5};
-		Arbitrator arby = new Arbitrator(bArray);
-		arby.start();		
 		
+		
+		LCD.drawString("White", 1, 1);
+
+		Button.ENTER.waitForPressAndRelease();
+
+		ls.calibrateLow();
+
+		LCD.clear();
+
+	LCD.drawString("Grey", 1, 1);
+
+		Button.ENTER.waitForPressAndRelease();
+
+		GREYCOLOR = ls.getLightValue();
+
+		LCD.clear();
+		
+		Button.ENTER.waitForPressAndRelease();
+		
+
+		
+		
+		Behavior b1 = new Forward();
+
+		Behavior b2 = new Rotate();
+
+		Behavior b3 = new DetectHoop();
+
+		Behavior b4 = new SeeHoop();
+
+		Behavior b5 = new Fire();
+
+		Behavior b6 = new VictoryCelebration();
+
+		Behavior[] bArray = { b1, b2, b3, b4, b5, b6 };
+
+		Arbitrator arby = new Arbitrator(bArray);
+
+		arby.start();
+
 	}
 
 }
--- DetectHoop.java
+++ DetectHoop.java
@@ -1,28 +1,50 @@
+import lejos.nxt.Sound;
 import lejos.robotics.subsumption.Behavior;
 
 public class DetectHoop implements Behavior {
 
-
 	public boolean takeControl() {
+
 		if (Basketball.us.getDistance() < 15)
+
 		{
-			
 			return true;
+
+		} else {
+			return false;
 		}
-		
-		return false;
+
 	}
 
 	public void action() {
-		
-		Basketball.power.stop();
-		Basketball.DETECTED  = true;
-	
- 
+		if (Basketball.DC == true) {
+			Basketball.power.quickStop();
+			if (Basketball.ls.getLightValue() < 25 && Basketball.WHITE == true) {
+				Basketball.power.stop();
+				Basketball.WHITE = false;
+				Basketball.DETECTED = true;
+				
+			} else if (((Basketball.ls.getLightValue() >= (Basketball.GREYCOLOR -15)) && (Basketball.ls.getLightValue() <= (Basketball.GREYCOLOR + 15))) && (Basketball.GREY == true))
+			{
+				Sound.buzz();
+				Basketball.power.stop();
+				Basketball.GREY = false;
+				Basketball.DETECTED = true;
+			}
+			else {
+				
+				Basketball.power.travel(-100);
+				Basketball.power.rotate(90);
+				
+			}
+			Basketball.DC = false;
+			
+			
+
+		}
 	}
 
 	public void suppress() {
-
 
 	}
 
